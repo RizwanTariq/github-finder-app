f3682d624617a7f53243779ef234aaf5a45bc34a
Rizwan Tariq <rizwantariq95@outlook.com>
2022-01-02 22:03:04 +0500

    Initialize dependencies & configrations.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 function App() {
-  return <div></div>;
+  return (
+    <div className="bg-purple-500">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <button className="btn">Click</button>
+    </div>
+  );
 }
 
 export default App;
