fd4b9fbd373ac3ae7c4d29c301763fc06732272f
Rizwan Tariq <rizwantariq95@outlook.com>
2022-01-04 00:01:58 +0500

    Built home,about, not-found page components & routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
+import { Route, Routes } from "react-router-dom";
 import Footer from "./components/layout/Footer";
 import NavBar from "./components/layout/NavBar";
+import About from "./pages/About";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
     <div className="flex flex-col justify-between h-screen">
       <NavBar />
-      <main className="container mx-auto px-3 pb-12">Content</main>
+      <main className="container mx-auto px-3 pb-12">
+        <Routes>
+          <Route path="/about" element={<About />} />
+          <Route path="/*" element={<NotFound />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </main>
       <Footer />
     </div>
   );
